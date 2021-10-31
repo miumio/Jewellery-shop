373995c83c910a2ede47b9e41ec5b4c1dd3b18c7
miumio <nickolskaya@gmail.com>
2021-10-31 23:29:14 +0200

    FEAT add tablet

--- build/js/main.js
+++ build/js/main.js
@@ -47,8 +47,10 @@
 })();
 
 (function () {
+  const body = document.querySelector('body');
+
   const header = document.querySelector('.page-header');
-  const nav = document.querySelector('.nav');
+  const nav = header.querySelector('.nav');
   const toggler = nav.querySelector('.nav__button');
 
   if (!header) return;
@@ -56,20 +58,17 @@
   nav.classList.remove('nav--nojs');
   nav.classList.add('nav--closed');
   toggler.classList.add('nav__button--show');
-  header.classList.add('page-header--white');
 
   toggler.addEventListener('click', function () {
-    if (
-      nav.classList.contains('nav--closed') &&
-      header.classList.contains('page-header--white')
-    ) {
+    if (nav.classList.contains('nav--closed'))
+      {
       nav.classList.remove('nav--closed');
       nav.classList.add('nav--opened');
-      header.classList.remove('page-header--white');
+      body.style.overflow = 'hidden';
+      nav.style.overflow = 'scroll';
     } else {
       nav.classList.add('nav--closed');
       nav.classList.remove('nav--opened');
-      header.classList.add('page-header--white');
     }
   });
 })();
