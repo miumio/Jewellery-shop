9923a780f017bee87945300d554111e69c9787c8
miumio <nickolskaya@gmail.com>
2021-11-01 20:01:07 +0200

    FIX js

--- build/js/main.js
+++ build/js/main.js
@@ -77,28 +77,34 @@
 })();
 
 (function () {
-if (!('Swiper' in window)) return;
+  if (!("Swiper" in window)) return;
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  loop: true,
-  navigation: {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      // renderBullet: function (index, className) {
+      //   return '<span class="' + className + '">' + (index + 1) + "</span>";
+      // },
+      dynamicBullets: true,
+      dynamicMainBullets: 3,
+      type: 'custom',
+      renderCustom: (swiper, current, total) => {
+        return `<span>${current} of ${total}</span>`
+      }
     },
-  },
-  breakpoints: {
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 50,
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
     },
-  }
-});
+  });
 })();
