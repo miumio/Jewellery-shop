8c3743d242163a939fa112a1ab692c80b978e93d
miumio <nickolskaya@gmail.com>
2021-11-07 13:22:34 +0200

    ADD img comment in insta

--- source/js/filter.js
+++ source/js/filter.js
@@ -1,12 +1,14 @@
 (function () {
   const link = document.querySelector('.catalog__filter-link');
   const filter = document.querySelector('.catalog__filter');
+
+  if (!filter) return;
+
   const form = filter.querySelector('form');
   const checkbox = form.querySelectorAll('input');
   const closeButton = filter.querySelector('.filter__button');
   const resetButton = form.querySelector('button[type=reset]');
 
-  if (!link) return;
 
   link.classList.add('catalog__filter-link--show');
 
